102aa49d6c3eb1c8e3f4804afcb094e6f39f8b02
fabio <fabiopitte@gmail.com>
2019-07-10 13:34:14 -0300

    fixed a bug in userViewModel

--- TestDFO.Application/ViewModels/UserViewModel.cs
+++ TestDFO.Application/ViewModels/UserViewModel.cs
@@ -15,9 +15,9 @@
             Address = address;
         }
 
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-        public int Age { get; private set; }
-        public string Address { get; private set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
     }
 }
